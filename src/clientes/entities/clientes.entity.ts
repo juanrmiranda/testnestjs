07862b8e3ea670bace0ad139ed282260f9836a5a
juanrmiranda <juanrmirand@gmaill.com>
2024-05-07 11:48:23 -0600

    add snakeCase to column definition

--- src/clientes/entities/clientes.entity.ts
+++ src/clientes/entities/clientes.entity.ts
@@ -4,9 +4,9 @@ import { Genero } from 'src/catalogos/entities/genero.entity';
 import { Profesion } from 'src/catalogos/entities/profesion.entity';
 import { TipoPersoneria } from 'src/catalogos/entities/tipoPersoneria.entity';
 import { TipoVivienda } from 'src/catalogos/entities/tipoVivienda.entity';
-import { tk_ManyToOneRestric, tk_ManyToOneRestricNullable } from 'src/tkcore/tk_RelationsRestriction.decorators';
+import { tk_ManyToOneRestricNullable } from 'src/tkcore/tk_RelationsRestriction.decorators';
 import { tk_AuditTable } from 'src/tkcore/tk_AuditTable';
-import {Entity,Column, ManyToOne, JoinColumn} from 'typeorm';
+import { Entity,Column } from 'typeorm';
 
 @Entity({name: "clientes", schema: "clientes"})
 export class Clientes extends tk_AuditTable{
@@ -17,32 +17,27 @@ export class Clientes extends tk_AuditTable{
     apellidos: string
 
     @Column({ type: "character varying", length: 200, nullable:true})
-    razon_social: string
+    razonSocial: string
 
     @Column({type: "date",nullable:true,name: "fecha_nacimiento"})
     fechaNacimiento: Date
     
-    @ManyToOne(type => Profesion,{onDelete: 'RESTRICT',onUpdate: 'RESTRICT'})
-    @JoinColumn({ name: 'profesion' })
-    @Column({name: 'profesion',nullable:true})
+    @tk_ManyToOneRestricNullable(() => Profesion)
     profesion: number
 
     @tk_ManyToOneRestricNullable(() => TipoVivienda)
-    tipo_vivienda: number
+    tipoVivienda: number
 
     @tk_ManyToOneRestricNullable(() => EstadoCliente)
     estado: number
 
-    @ManyToOne(type => Genero,{onDelete: 'RESTRICT',onUpdate: 'RESTRICT'})
-    @JoinColumn({ name: 'genero' })
+    @tk_ManyToOneRestricNullable(() => Genero)
     genero: number
     
-    @ManyToOne(type => TipoPersoneria,{onDelete: 'RESTRICT',onUpdate: 'RESTRICT'})
-    @JoinColumn({ name: 'tipo_personeria' })
+    @tk_ManyToOneRestricNullable(()=> TipoPersoneria)
     tipoPersoneria: number
 
-    @ManyToOne(type => EstadoCivil,{onDelete: 'RESTRICT',onUpdate: 'RESTRICT'})
-    @JoinColumn({ name: 'estado_civil' })
+    @tk_ManyToOneRestricNullable(() => EstadoCivil)
     estadoCivil: number
 
     @Column({type: "character varying",nullable:true, length: 9})
--- src/tkcore/tk_RelationsRestriction.decorators.ts
+++ src/tkcore/tk_RelationsRestriction.decorators.ts
@@ -1,21 +1,24 @@
 import { ManyToOne, JoinColumn, Column } from 'typeorm';
+import { snakeCase } from 'lodash';
 
 // Define una función de fábrica para generar decoradores personalizados
 function ManyToOneRestricNullable() {
     return function (type: any, name?: string) {
         return function (target: any, propertyName: string) {
+            const columnName = name ? name : snakeCase(propertyName);
             ManyToOne(type, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT' })(target, propertyName);
-            JoinColumn({ name:  name ?? propertyName.toLowerCase() })(target, propertyName);
-            Column({ name: name ?? propertyName.toLowerCase(), nullable: true })(target, propertyName);
+            JoinColumn({ name:  columnName })(target, propertyName);
+            Column({ name: columnName, nullable: true })(target, propertyName);
         };
     };
 }
 function ManyToOneRestric() {
     return function (type: any, name?: string) {
         return function (target: any, propertyName: string) {
+            const columnName = name ? name : snakeCase(propertyName);
             ManyToOne(type, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT' })(target, propertyName);
-            JoinColumn({ name:  name ?? propertyName.toLowerCase() })(target, propertyName);
-            Column({ name: name ?? propertyName.toLowerCase() })(target, propertyName);
+            JoinColumn({ name:  columnName })(target, propertyName);
+            Column({ name: columnName })(target, propertyName);
         };
     };
 }
